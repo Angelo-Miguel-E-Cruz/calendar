2b088229a760aa9593ccac1fe7aad58ec697e84f
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-09-02 23:08:42 +0800

    quick fix modal text

--- src/components/modals/inviteUserModal.tsx
+++ src/components/modals/inviteUserModal.tsx
@@ -25,7 +25,7 @@ export default function InviteUser({ isOpen, onClose, userEmail, handleSubmit, h
             </div>
             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
               <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                Delete Event
+                Invite User
               </DialogTitle>
               <div className="mt-2">
                 <div className="mt-2">
